54e4a61731234cd18891b58583407b4b5a0730ef
Данило Гнатюк <danilognatuk@MacBook-Air-Danilo.local>
2023-05-29 13:18:12 +0300

    first commit

---
+++ src/test/java/com/example/SDPK/SdpkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SDPK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdpkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
